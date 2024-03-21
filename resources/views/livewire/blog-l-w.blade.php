9ddbfd26ceb5130afc52ef99309580a1413b9a70
Joseph Neil Osorio <joseph.osorio@gmail.com>
2024-03-21 17:33:01 +0800

    run composer update

--- resources/views/livewire/blog-l-w.blade.php
+++ resources/views/livewire/blog-l-w.blade.php
@@ -3,7 +3,7 @@
 
     <div class="grid grid-cols-1 sm:grid-cols-2 sm:portrait:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-5 lg:gap-14 xl:gap-16">
         @foreach($posts as $post)
-            <article class="bg-white flex flex-col justify-items-center mb-10 xs:p-5">
+            <article class="flex flex-col justify-items-center mb-10 xs:p-5 bg-white">
                 <figure class="rounded-lg cursor-pointer overflow-hidden aspect-w-1 aspect-h-1 max-w-full h-72 size-full mb-10">
                     <img src="{{$post->image}}" alt="" class="object-cover w-full h-full">
                 </figure>
@@ -12,7 +12,7 @@
                     <p class="text-base uppercase justify-self-start text-doveGray-50">{{ strtoupper($post->created_at->format('F d, Y')) }} - {{ $post->author }}</p>
                 </div>
                 <p class="mb-5 text-base leading-6 text-doveGray-200">{{$post->content}}</p>
-                <a href="#" class="read-more block font-bold text-black/75 hover:text-black/30 transition-all duration-200">
+                <a href="#" class="read-more block font-bold text-black/75  hover:text-black/30 transition-all duration-200">
                     Read more
                     <span class="revert-layer after:content-['\2192'] pl-1 font-normal text-sm transform-none tracking-normal"></span>
                 </a>
@@ -21,9 +21,9 @@
     </div>
 
     <div class="pagination flex justify-center">
-        <a href="/blog?offset=1687258800296" class="flex justify-center items-center font-normal text-xs text-[#999]">
+        <a href="/blog?offset=1687258800296" class="flex justify-center items-center font-normal text-xs text-doveGray-200">
             Older
-            <span class="ml-2 text-base text-[#ccc] opacity-6"><i class="fa fa-chevron-right" aria-hidden="true"></i></span>
+            <span class="ml-2 text-base opacity-6 text-doveGray-50"><i class="fa fa-chevron-right" aria-hidden="true"></i></span>
         </a>
     </div>
 </section>
